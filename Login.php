74aa77f655ee047953599ede344ff82054ec75a5
Livia-02 <liviabenevente02@gmail.com>
2025-05-21 08:45:14 -0300

    Personalizar

---
+++ Login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-8 col-md-6">
+             <h2>Login do Sistema</h2><br>
+               <div class="form-group">
+                 <label for="formGroupExampleLogin">Login</label>
+                 <input type="text" class="form-control" id="formGroupExampleLogin">
+                </div>
+                 <div class="form-group">
+                 <label for="formGroupExamplePassword">Password</label>
+                 <input type="text" class="form-control" id="formGroupExamplePassword"><br>
+                 <button type="button" class="btn btn-outline-primary">Acessar</button>
+                </div>
+             </form> 
+        </div>
+    </div>
+</div>
+</body>
+</html>
