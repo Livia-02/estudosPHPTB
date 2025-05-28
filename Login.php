403cc50a00b401d759d9b31edeb2ac529a82e125
Livia-02 <liviabenevente02@gmail.com>
2025-05-28 08:49:46 -0300

    Continuação

--- Login.php
+++ Login.php
@@ -5,24 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-12 col-sm-8 col-md-6">
-             <h2>Login do Sistema</h2><br>
-               <div class="form-group">
-                 <label for="formGroupExampleLogin">Login</label>
-                 <input type="text" class="form-control" id="formGroupExampleLogin">
-                </div>
-                 <div class="form-group">
-                 <label for="formGroupExamplePassword">Password</label>
-                 <input type="text" class="form-control" id="formGroupExamplePassword"><br>
-                 <button type="button" class="btn btn-outline-primary">Acessar</button>
-                </div>
-             </form> 
+    
+    <div class="container mt-5 d-flex login justify-content-center align-items-center">
+        <div class="card w-100" style="max-width: 400px;">
+            <div class="card-body fundoroxo">
+            <header class="text-center text-white p-4 ">
+                <small class="text-muted">Login do Sistema</small>
+            </header>
+                <form>
+                    <div class="form-group">
+                        <label for="formGroupExampleLogin">Login</label>
+                        <input type="text" class="form-control" id="formGroupExampleLogin">
+                    </div>
+                    <div class="form-group mt-3">
+                        <label for="formGroupExamplePassword">Senha</label>
+                        <input type="password" class="form-control" id="formGroupExamplePassword">
+                    </div>
+                    <div class="mt-4">
+                        <button type="submit" class="btn btn-outline-light w-100">Acessar</button>
+                    </div>
+                </form>
+            </div>
+            <div class="card-footer text-center">
+                <small class="text-muted">Essa página é reservada.</small>
+            </div>
         </div>
     </div>
-</div>
 </body>
 </html>
---
+++ Resposta.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session php</title>
+</head>
+<body class="bg-light">
+    <div class="container d-flex justify-content-center min-vh-100 mt-5">
+
+    
+    
+    
+</body>
+</html>
