4a93db0c87b7c0ce471edf1fdd32e1c530aa04e9
Livia-02 <liviabenevente02@gmail.com>
2025-06-04 10:49:20 -0300

    Trabalho

--- Login.php
+++ Login.php
@@ -15,14 +15,14 @@
             <header class="text-center text-white p-4 ">
                 <div class="text-center"><h2>Login do Sistema</h2></div>
             </header>
-                <form>
+            <form action="Resposta.php" method="POST">
                     <div class="form-group">
-                        <label for="formGroupExampleLogin">Login</label>
-                        <input type="text" class="form-control" id="formGroupExampleLogin">
+                    <label for="username">Nome de Usuário</label>
+            <input type="text" class="form-control" name="usuario" id="usuario" placeholder="Seu nome de usuário">
                     </div>
                     <div class="form-group mt-3">
-                        <label for="formGroupExamplePassword">Senha</label>
-                        <input type="password" class="form-control" id="formGroupExamplePassword">
+                    <label for="password">Senha</label>
+            <input type="password" class="form-control"name="senha" id="senha"  placeholder="Senha">
                     </div>
                     <div class="mt-4">
                         <button type="submit" class="btn btn-outline-light w-100">Acessar</button>
@@ -33,6 +33,21 @@
                 <small class="text-muted">Essa página é reservada.</small>
             </div>
         </div>
+        <?php
+    if (isset($_GET['return'])) {
+      echo '
+      <div class="row justify-content-end fixed-bottom">
+        <div class="col-12 col-sm-6 col-md-4 ml-2 mr-2">
+          <div class="alert alert-warning p-3">
+            <h4 class="text-center">'
+              . $_GET['return'] .
+            '</h4>
+          </div>
+        </div>
+      </div>
+      ';
+    }
+    ?>
     </div>
 </body>
 </html>
--- Sistema.php
+++ Sistema.php
@@ -1,11 +1,6 @@
 <?php
-if($_POST['usuario']  == 'admin' && $_POST['senha'] == 'senha'){
-  session_start();
-  $_SESSION['usuario'] = $_POST['usuario'];
-}else{
-  // echo"<script> alert('Opa... deu merda!' ) </script>";
-  header('location: Login.php?return=Opa deu merda!!!');
-}
+session_start();
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -14,7 +9,6 @@ if($_POST['usuario']  == 'admin' && $_POST['senha'] == 'senha'){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
   <link rel="stylesheet" href="bootstrap.css">
-
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         crossorigin="anonymous">
@@ -22,19 +16,20 @@ if($_POST['usuario']  == 'admin' && $_POST['senha'] == 'senha'){
           integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
           crossorigin="anonymous" defer></script>
 </head>
-<body class="bg-light">
-
-  <div class="container d-flex justify-content-center  min-vh-100 mt-5">
-
-  <div class="alert alert-info " role="alert">
-    <?php
-    echo "<h4 class='alert-heading mt-3 mr-5 ml-5'>Seja bem vindo ao sistema ". $_SESSION['usuario'] ."!</h4>";
-    ?>
-     <form action="Sistema.php" method="POST">
-  <button type="submit" class="btn btn-outline-dark btn-block mt-4 mb-4">Ir para a proxima pagina</button>
-</form>
+<body>
+<div class="container mt-2">
+    <div class="row justify-content-center">
+    <div class="col-12 col-sm-8 col-md-6">
+        <div class="alert alert-info mt-3">
+            <h4 class="text-center">
+             Dashboard do Sistema
+             <?php
+           echo "<h4 class='alert-heading mt-3 mr-5 ml-5 text-center'>Bem vindo ao sistema ". $_SESSION['usuario'] ."!</h4>";
+             ?>
+            </h4>
+        </div>
+    </div>
 </div>
-  </div>
 
 </body>
 </html>
